5ac3891db3054e1e96e04eef584e347f541ad603
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-09-29 19:29:39 +0300

    Update 29.09.2023

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,7 +21,9 @@ export const config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
-    specs: ['./test/specs/**/*.js'],
+    specs: [
+        './test/specs/**/*.js'
+    ],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
@@ -133,30 +135,30 @@ export const config = {
         timeout: 60000
     },
 
-    cucumberOpts: {
-        // <string[]> (file/dir) require files before executing features
-        require: ['./features/**/steps.js'],
-        // <boolean> show full backtrace for errors
-        backtrace: false,
-        // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
-        requireModule: [],
-        // <boolean> invoke formatters without executing steps
-        dryRun: false,
-        // <boolean> abort the run on first failure
-        failFast: false,
-        // <boolean> hide step definition snippets for pending steps
-        snippets: true,
-        // <boolean> hide source uris
-        source: true,
-        // <boolean> fail if there are any undefined or pending steps
-        strict: false,
-        // <string> (expression) only execute the features or scenarios with tags matching the expression
-        tagExpression: '',
-        // <number> timeout for step definitions
-        timeout: 60000,
-        // <boolean> Enable this config to treat undefined definitions as warnings.
-        ignoreUndefinedDefinitions: false
-    },
+    // cucumberOpts: {
+    //     // <string[]> (file/dir) require files before executing features
+    //     require: ['./test/features/**/steps.js'],
+    //     // <boolean> show full backtrace for errors
+    //     backtrace: false,
+    //     // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    //     requireModule: [],
+    //     // <boolean> invoke formatters without executing steps
+    //     dryRun: false,
+    //     // <boolean> abort the run on first failure
+    //     failFast: false,
+    //     // <boolean> hide step definition snippets for pending steps
+    //     snippets: true,
+    //     // <boolean> hide source uris
+    //     source: true,
+    //     // <boolean> fail if there are any undefined or pending steps
+    //     strict: false,
+    //     // <string> (expression) only execute the features or scenarios with tags matching the expression
+    //     tagExpression: '',
+    //     // <number> timeout for step definitions
+    //     timeout: 60000,
+    //     // <boolean> Enable this config to treat undefined definitions as warnings.
+    //     ignoreUndefinedDefinitions: false
+    // },
 
     
     //
